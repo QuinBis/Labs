718084e1a0bf08a689e4e946418b6b5b034eadb7
1 <1@LAPTOP-2DTST51R.local>
2023-09-22 10:22:21 +0700

    Добавлен проект для лабораторной №2.

---
+++ ASD_Lab2/Library.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+#include "Header.h"
+
+//	proverka
+// проверка
---
+++ ASD_Lab2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "Header.h"
+
+int main(){
+	setlocale(0,"Russian");
+
+
+	// проверка.
+	// proverka.
+
+
+}
