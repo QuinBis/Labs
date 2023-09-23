2b391f717c4e0409e05f0cd11b4030bd8a422041
1 <1@LAPTOP-2DTST51R.local>
2023-09-23 15:43:44 +0700

    Реализован алгоритм Боуера-Мура.
    Сам алгоритм уложился в 1 цикл.
    В цикле идем от [ lengthWord - 1 ; lengthText)
    Первая проверка на то , что индекс j >=0 , в противном случае (когда слово было полностью найдено и j стало равно -1) мы сообщаем пользователю о том, что слово было найдено и индекс начала вхождения.
    Сравниваются два символа, если они равны, смещаемся в слове и в тексте на -1.
    Если символы не равны, прибавляем к i удаленность образа , где индексом является сама буква.
    Индекс хождения по слову (j) возвращаемся на конец слова (lengthWord - 1).
    А индекс k (дополнительная переменная - индекс, для хождения по слову) снова делаем равным i (уже со смещением).

--- ASD_Lab2/main.cpp
+++ ASD_Lab2/main.cpp
@@ -7,8 +7,6 @@
 
 
 
-
-
 int main() {
 
 	setlocale(0, "Russian");
@@ -29,9 +27,51 @@ int main() {
 		table[word[i]] = lengthWord - 1 - i;
 	}
 
-	// testing...
-	for (int i = 0; i < lengthWord; i++)
-		std::cout << word[i] << " = " << table[word[i]] << std::endl;
+
+	// implementation of the BM algorithm.
+
+
+	int k = lengthWord - 1;
+
+	for (int i = lengthWord - 1 , j = lengthWord - 1; i < lengthText;) { 
+
+
+		if (j >= 0) {
+
+			// the symbols matched.
+			if (word[j] == text[k]) {
+				std::cout << "The symbol of the word " << word[j] << " coincided with the symbol of the text " << text[k] << std::endl;
+				j--;k--;
+			}
+
+			// the symbols didn't match.
+			else { 
+				std::cout << text[k] << std::endl;
+				i = i + table[text[k]];
+				j = lengthWord - 1;
+				k = i;
+			}
+
+		}
+
+		else {
+
+			std::cout << "The word has been found!" << std::endl;
+			std::cout << "The word : " << word << std::endl;
+			std::cout << "The text : " << text << std::endl;
+			std::cout << "Index of the beginning of the occurrence : " << i;
+			i = lengthText; // exiting the function.
+
+		}
+		
+	}
+
+
+
+
+
+
+
 
 
 	return 0;
