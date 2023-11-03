a0301b0c8303228943d9241f35b5113606b56f02
reqw <reqw@DESKTOP-6U8LISL.local>
2023-11-03 14:08:02 +0700

    Создан проект для Побитовой сортировки по АИСД

---
+++ ASD_Lab6/Library.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <assert.h>
+#include <vector>
+#include <random>
+#include <string>
+
+#include "Header.h"
---
+++ ASD_Lab6/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <assert.h>
+
+#include "Header.h"
+
+int main() {
+	std::cout << "Hello sort!" << std::endl;
+	return 0;
+}
